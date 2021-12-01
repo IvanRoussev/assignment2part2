b41279eb68c3ae99904740e424ff314a68f09671
Ivan <ivan.roussev12@gmail.com>
2021-11-30 19:49:40 -0800

    Finished up to part 9 created all function working somewhat properly so far

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+function cleanUpView() {
+    let viewclass  = document.querySelector('.main')
+    viewclass.innerHTML = ''
+}
+
+
+
+function cleanUpCreate(){
+	let page2 = document.querySelector('div.contactedit')
+	page2.innerHTML= ''
+}
+
+
+function CleanFunctionIndex() {
+	let mainClass = document.querySelector('.main');
+	
+   
+	while (mainClass.firstChild) {
+	  mainClass.removeChild(mainClass.firstChild);
+	}
+  }
+  
+
 const Contact_key = document.getElementById("contactshome");
 
 function Contact_change(evt){
@@ -8,6 +31,9 @@ function Contact_change(evt){
 };
 
 Contact_key.addEventListener("click", Contact_change)
+
+
+
 
 
 
@@ -24,15 +50,6 @@ function create_Contact_key(evt){
 
 CreateContact.addEventListener("click", create_Contact_key)
 
-
-function CleanFunctionIndex() {
-  let mainClass = document.querySelector('.main');
-  
- 
-  while (mainClass.firstChild) {
-    mainClass.removeChild(mainClass.firstChild);
-  }
-}
 
 function createSingleIndex(contact) {
   let div1 = document("div");
@@ -52,6 +69,27 @@ function createSingleIndex(contact) {
   return div1;
 }
 
+function renderView() {
+	const section = document.querySelector(".main");
+	section.insertAdjacentHTML("afterbegin", page3_String );
+	
+	
+	const edit_button = document.querySelector(".edit");
+	function edit_butt(evt){
+	  evt.preventDefault();
+	  evt.stopImmediatePropagation();
+	  alert("nothing");
+	};
+  
+	edit_button.addEventListener("click", edit_butt)
+
+	const Clost_butt = document.querySelector(".close");
+	function close_button(){	  
+		cleanUpView();
+	    renderIndex(contactList);
+	};
+
+	Clost_butt.addEventListener("click", close_button)
 
 function renderIndex(contactList) {
   let mainClass = document.querySelector(".main");
@@ -61,13 +99,24 @@ function renderIndex(contactList) {
   }
 }
 
+page3_String =  `<div class="contactinfo">
+	<div class="contactname">
+		Raquel Ervin
+		<img src="./img/profile.jpg" class="profilepic" alt="Profile picture">
+	</div>
+	<div class="contactemail">email: rocket@gmail.com</div>
+	<div class="contactphone">cell: +1 555 555-5555</div>
+	<div class="contactaddress">address: 123 front st, Unit #1, Dakota City</div>
+	<div class="buttons">
+		<button class="button edit" value="Edit">Edit</button>
+		<button class="button close" value="Close">Close</button>
+	</div>
+	</div>
+	</div>`
 
-function cleanUpCreate(){
-	let page2 = document.querySelector('div.contactedit')
-	page2.innerHTML= ''
-}
 
-Massive_string = `<div class="contactedit">
+
+page2_string = `<div class="contactedit">
 <div class="contactimg">
   <img src="./img/profile.jpg" class ="profilepic" alt="Profile picture">
 </div>
@@ -92,3 +141,52 @@ Massive_string = `<div class="contactedit">
   <button type="reset" class="button cancel" id="cancel" name="cancel">Cancel</button>
 </div>`
 
+
+function renderCreate() {
+	const section = document.querySelector(".main");
+    section.insertAdjacentHTML("afterbegin", page2_string);
+
+    const save_button = document.querySelector(".save");
+    function addCleanRender(evt){
+		evt.preventDefault();
+    
+	    info = [];
+        let getInfo = document.querySelectorAll(".inputcontainer");
+        for (let i = 0; i < getInfo.length; i++) {
+			info.push(getInfo[i].firstElementChild.value);}
+            addInfo(info);
+            cleanUpCreate();
+            renderIndex(contactList);
+		};
+  
+  		save_button.addEventListener("click", addCleanRender)
+  
+		const cancel_butt = document.getElementById("cancel");
+  		function cancel_button(evt){
+    		evt.preventDefualt();
+    		cleanUpCreate();
+    		renderIndex(contactList);
+   		cancel_butt.addEventListener("click", cancel_button)
+
+  };
+}
+
+
+
+
+}
+
+const contactList = [  
+	{ 
+		name: "Oliver Queen", 
+		phone: "778-555-1234", 
+		address: "101 Main St, Star City, USA",    
+		email: "greenarrow@watchtower.com",  
+	},   
+	{    
+		name: "Jessica Cruz",    
+		phone: "123-555-5555",    
+		address: "Portland Oregon",    
+		email: "greenlantern@watchtower.com",  
+	}
+]
